bf330fc99aea4cd5d8764165a5cae4423e7be9f9
amasson42 <amasson@student.42.fr>
2021-01-14 21:38:18 +0100

    Initial Commit

---
+++ Shared/SilvesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilvesApp.swift
+//  Shared
+//
+//  Created by Giantwow on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SilvesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
